b3d3009ee33f94cee69ceb43ff9d6530a7fe4380
Diego Sieg <diegosieg@gmail.com>
2025-04-20 13:47:09 +1200

    Add basic Next.js project with TypeScript

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <h1>Rick and Morty Explorer</h1>
+    </div>
+  );
+}
